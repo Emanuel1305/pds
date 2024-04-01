aab064aec93351e16604b97c2629a8fc28f4b994
Emanuel Fagundes <emanuelfafa1313@gmail.com>
2024-04-01 10:18:53 -0400

    l

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 let trilho = document.getElementById('trilho')
 let body = document.querySelector('body')
-let logo = document.querySelector('#logo')
 
 trilho.addEventListener('click', ()=>{
     trilho.classList.toggle('dark')
     body.classList.toggle('dark')
 })
+
+
